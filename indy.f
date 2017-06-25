c28011c48179e94f5a152a978f82a5e97fbfd5ae
Curtis McEnroe <programble@gmail.com>
2017-06-25 13:27:53 -0400

    Implement file I/O

--- indy.f
+++ indy.f
@@ -584,7 +584,49 @@
 
 \ File I/O.
 
-( TODO )
+: R/O ( -- fam ) O_RDONLY ;
+: R/W ( -- fam ) O_RDWR ;
+
+: OPEN-FILE ( str len fam -- fd errno )
+    -ROT
+    CSTRING
+    SYS_OPEN SYSCALL2
+    DUP
+    DUP 0< IF
+        NEGATE
+    ELSE
+        DROP 0
+    THEN
+;
+
+: CREATE-FILE ( str len fam -- fd errno )
+    O_CREAT O_TRUNC OR OR
+    -ROT
+    CSTRING
+    420 -ROT
+    SYS_OPEN SYSCALL3
+    DUP
+    DUP 0< IF
+        NEGATE
+    ELSE
+        DROP 0
+    THEN
+;
+
+: CLOSE-FILE ( fd -- errno ) SYS_CLOSE SYSCALL1 NEGATE ;
+
+: READ-FILE ( str len fd -- rlen errno )
+    >R SWAP R>
+    SYS_READ SYSCALL3
+    DUP
+    DUP 0< IF
+        NEGATE
+    ELSE
+        DROP 0
+    THEN
+;
+
+: PERROR ( errno str len -- ) TELL ." : ERRNO=" . CR ;
 
 \ Assembler.
 
