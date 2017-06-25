b4fca9d85d3f16563c42ca10653745b6dea1c10d
Curtis McEnroe <programble@gmail.com>
2017-06-25 13:08:06 -0400

    Add FreeBSD open mode constants

--- freebsd.asm
+++ freebsd.asm
@@ -12,3 +12,12 @@ SYS_READ:  equ 3
 SYS_WRITE: equ 4
 SYS_OPEN:  equ 5
 SYS_CLOSE: equ 6
+
+O_RDONLY:   equ 0000h
+O_WRONLY:   equ 0001h
+O_RDWR:     equ 0002h
+O_NONBLOCK: equ 0004h
+O_APPEND:   equ 0008h
+O_CREAT:    equ 0200h
+O_TRUNC:    equ 0400h
+O_EXCL:     equ 0800h
